a889e4f49a60a30ddebf967d2b628a122ffc8850
Anthony Hernandez <antherdz09@gmail.com>
2026-04-24 09:31:06 -0700

    new

--- app/src/main/java/com/example/timewellspent/SessionListActivity.kt
+++ app/src/main/java/com/example/timewellspent/SessionListActivity.kt
@@ -7,8 +7,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.backendless.Backendless
+import com.backendless.BackendlessUser
 import com.backendless.async.callback.AsyncCallback
 import com.backendless.exceptions.BackendlessFault
+import com.backendless.persistence.DataQueryBuilder
 import com.example.timewellspent.databinding.ActivitySessionListBinding
 
 
@@ -31,19 +33,38 @@ class SessionListActivity : AppCompatActivity() {
 
         // make backendless call to retrieve all data
         // in the handleResponse, get the list of data and constructor the adapter & apply to the reyclerview
-        Backendless.Data.of<Session>(Session::class.java)
-            .find(object : AsyncCallback<List<Session>> {
-                override fun handleResponse(foundSessions: List<Session>) {
-                    adapter = SessionAdapter(foundSessions)
-                    binding.recyclerViewActivitySession.adapter = adapter
-                    binding.recyclerViewActivitySession.layoutManager = LinearLayoutManager(this@SessionListActivity)
 
-                }
+        Backendless.UserService.CurrentUser(true, object : AsyncCallback<BackendlessUser?> {
+            override fun handleResponse(response: BackendlessUser?) {
+                var userID = response?.userId
+                val whereClause = "ownerId = '${userID}'"
+                val queryBuilder = DataQueryBuilder.create()
+                queryBuilder.setWhereClause(whereClause)
+                Backendless.Data.of<Session>(Session::class.java).find(
+                    queryBuilder,
+                    object : AsyncCallback<List<Session>> {
+                        override fun handleResponse(foundSessions: List<Session>) {
+                            adapter = SessionAdapter(foundSessions)
+                            binding.recyclerViewActivitySession.adapter = adapter
+                            binding.recyclerViewActivitySession.layoutManager = LinearLayoutManager(this@SessionListActivity)
+                        }
 
-                override fun handleFault(fault: BackendlessFault) {
-                    // an error has occurred, the error code can be retrieved with fault.getCode()
-                }
-            })
-    }
+                        override fun handleFault(fault: BackendlessFault) {
+                            // an error has occurred, the error code can be retrieved with fault.getCode()
+                        }
+                    })
+
+            }
+
+            override fun handleFault(fault: BackendlessFault) {
+                // error handling logic
+            }
+        })
+
+
+
+        }
+
 
 }
+
